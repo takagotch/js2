9b858669a12f7e75fa88a1fedf3e0ca70d8f8f6f
takagotch <dyaccb@gmail.com>
2018-04-13 18:36:16 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- fr/objp.js
+++ fr/objp.js
@@ -7,8 +7,13 @@ myObject.myString = 'string';
 myObject.myNumber = 33;
 myObject.myDate = new Date();
 myObject.myRegExp = /a/;
+myObject.myNull = null;
+myObject.myUndefined = undefined;
+myObject.myObject = {};
+myObject.myMath_PI = Math.PI;
+myObject.myError = new Error('Error!');
 
-
+console.log(myObject.myFunction,myObject.myArray,);
 
 
 //
