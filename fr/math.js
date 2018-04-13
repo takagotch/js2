794f62d420c920448ffab2f2728c13d749f2496e
takagotch <dyaccb@gmail.com>
2018-04-13 18:27:12 +0900

    Vibrator v = (Vibrator) getSystemServie(VIBRATOR_SERVICE);

--- fr/math.js
+++ fr/math.js
@@ -1,9 +1,22 @@
 //
+PI
+SORT1_2
+SORT2
 
-//
-
-//
-
-//
+abs(x)
+acos(x)
+asin(x)
+...
+exp(x)
+floor(x)
+log(x)
+max([value1 [, value2 [, ...]]])
+min([value1 [, value2 [, ...]]])
+pow(x, y)
+random()
+round(x)
+sin(x)
+sqrt(x)
+tan(x)
 
 
